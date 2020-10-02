aa243ca506de19bfd76624587a65ec992bf133ac
Ravi Varshney <ravivarshney49@gmail.com>
2020-10-02 10:18:04 +0530

    receive notes from API and display them

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from 'react'
+import { API } from 'aws-amplify'
+import { List } from 'antd'
+import 'antd/dist/antd.css'
+import { listNotes } from './graphql/queries'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {
+  notes: [],
+  loading: true,
+  error: false,
+  form: { name: '', description: '' }
 }
 
-export default App;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_NOTES':
+      return { ...state, notes: action.notes, loading: false }
+    case 'ERROR':
+      return { ...state, loading: false, error: true }
+    default:
+      return state
+  }
+}
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const fetchNotes = async () => {
+    try {
+      const notesData = await API.graphql({
+        query: listNotes
+      })
+      console.log(notesData)
+      dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items })
+    }
+    catch (err) {
+      console.log('error: ', err)
+      dispatch({ type: 'ERROR' })
+    }
+  }
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+  return (
+    <div style={styles.container}>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
+    </div>
+  )
+}
+
+const renderItem = (item) => {
+  return (
+    <List.Item style={styles.item}>
+      <List.Item.Meta
+        title={item.name}
+        description={item.description}
+      />
+    </List.Item>
+  )
+}
+
+const styles = {
+  container: { padding: 20 },
+  input: { marginBottom: 10 },
+  item: { textAlign: 'left' },
+  p: { color: '#1890ff' }
+}
+export default App
